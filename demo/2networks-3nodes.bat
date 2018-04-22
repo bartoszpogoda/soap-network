b4a807abba53bce3dc4b81f07a02ec361b3957c3
bartoszpogoda <bartek.pogoda1@gmail.com>
2018-04-22 15:52:04 +0200

    demo cfg fix

--- demo/2networks-3nodes.bat
+++ demo/2networks-3nodes.bat
@@ -2,7 +2,7 @@
 start javaw -jar router-node.jar --nodePort=5060 --nodeId="a-R" --nextNetworkNodePort=5061 --nextRouterNodePort=5070
 start javaw -jar router-node.jar --nodePort=5070 --nodeId="b-R" --nextNetworkNodePort=5071 --nextRouterNodePort=5060
 
-start javaw -jar simple-node.jar --nodePort=5061 --nodeId="a-1" --nextNodePort=5062 --nextNodeHost="192.168.0.2"
+start javaw -jar simple-node.jar --nodePort=5061 --nodeId="a-1" --nextNodePort=5062
 start javaw -jar simple-node.jar --nodePort=5062 --nodeId="a-2" --nextNodePort=5060
 start javaw -jar simple-node.jar --nodePort=5071 --nodeId="b-1" --nextNodePort=5072
 start javaw -jar simple-node.jar --nodePort=5072 --nodeId="b-2" --nextNodePort=5070
